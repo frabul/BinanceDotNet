d2fd1a3dc839fd76cf5a70830ac094ecf04ee4f9
Francesco Buldo <francesco.buldo@fimer.com>
2022-02-27 09:34:14 +0100

    Modify BinanceBadRequestException so that error code and error message are included in exception message

--- BinanceExchange.API/Models/Response/Error/BinanceBadRequestException.cs
+++ BinanceExchange.API/Models/Response/Error/BinanceBadRequestException.cs
@@ -4,7 +4,7 @@
     /// This exception is used when malformed requests are sent to the server. Please review the request object
     /// </summary>
     public class BinanceBadRequestException : BinanceException {
-        public BinanceBadRequestException(BinanceError errorDetails) : base((string) "Malformed requests are sent to the server. Please review the request object/string", errorDetails)
+        public BinanceBadRequestException(BinanceError errorDetails) : base((string) $"Binance replied with error: {errorDetails.Code} {errorDetails.Message}", errorDetails)
         {
         }
     }
