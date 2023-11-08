15a502f1663572037d23126cf2f4ff822e342753
F.Buldo <f.buldo@riello-ups.com>
2023-11-08 18:06:50 +0100

    Fix deserialization of ExchangeInfoSymbolFilterNotional

--- BinanceExchange.API/Models/Response/ExchangeInfoSymbolFilterNotional.cs
+++ BinanceExchange.API/Models/Response/ExchangeInfoSymbolFilterNotional.cs
@@ -1,11 +1,18 @@
-﻿namespace BinanceExchange.API.Models.Response
+﻿using System.Runtime.Serialization;
+namespace BinanceExchange.API.Models.Response
 {
+    [DataContract]
     public class ExchangeInfoSymbolFilterNotional : ExchangeInfoSymbolFilter
     {
-        public decimal MinNotional { get; set; }
+        [DataMember]
+        public decimal MinNotional { get; set; } = -1;
+        [DataMember]
         public decimal MaxNotional { get; set; }
+        [DataMember]
         public bool ApplyMinToMarket { get; set; }
+        [DataMember]
         public bool ApplyMaxToMarket { get; set; }
+        [DataMember]
         public decimal AvgPriceMins { get; set; }
     }
 }
