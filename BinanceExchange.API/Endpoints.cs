9b5a1297534a9f632c2cddf27c127565d898934e
Francesco Buldo <frbuldo@gmail.com>
2025-01-18 14:13:41 +0100

    Fix Endpoints initialization

--- BinanceExchange.API/Endpoints.cs
+++ BinanceExchange.API/Endpoints.cs
@@ -75,6 +75,9 @@ namespace BinanceExchange.API
             {
                 RestApiDomain = RestApiDomain
             };
+            Other = new Other{
+                RestApiDomain = RestApiDomain
+            };
         }
 
 
